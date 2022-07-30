2d394e52162802858fa2659603262883568a3c2e
Ethan Petuchowski <etahnp@gmail.com>
2022-07-30 01:04:28 -0500

    Read-in the transactions. (disabled sandbox due to https://stackoverflow.com/a/67797686/1959155)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(EmptyApp());
@@ -8,10 +10,49 @@ class EmptyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Empty app')),
-        body: const Center(
-          child: Text('App booted successfully'),
+        body: Center(
+          child: Column(
+            children: [
+              const Text('App booted successfully'),
+              FutureBuilder<List<List<String>>>(
+                future: CopilotExportReader.loadData,
+                builder: (ctx, snapshot) => snapshot.hasData
+                    ? Text(snapshot.data!.length.toString())
+                    : const CircularProgressIndicator(),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+class CopilotExportReader {
+  static Future<List<List<String>>> get loadData async {
+    try {
+      final file = File('/Users/Ethan/Downloads/transactions.csv');
+      print('Reading in the file');
+      final contents = await file.readAsString();
+      print('Parsing the file');
+      return _parse(contents);
+    } catch (e) {
+      throw FileReadError('Uh oh! $e');
+    }
+  }
+
+  static List<List<String>> _parse(String filetext) {
+    return filetext
+        .split('\n')
+        .skip(1) // Skip header.
+        .map(_cleanup)
+        .map((line) => line.split(','))
+        .toList();
+  }
+
+  static String _cleanup(String line) => line;
+}
+
+class FileReadError extends StateError {
+  FileReadError(String message) : super(message);
 }
