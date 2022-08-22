5dab8cbffe3e20b7dfaec6cd0685257c7b265213
Ethan Petuchowski <etahnp@gmail.com>
2022-08-22 17:41:14 -0500

    Fixed big bug in line chart tooltip

--- lib/charts/line/my_line_chart.dart
+++ lib/charts/line/my_line_chart.dart
@@ -94,11 +94,11 @@ class _Chart extends StatelessWidget {
         maxContentWidth: 200, // default is 120.
         getTooltipItems: (touchedSpots) {
           return touchedSpots.mapWithIdx((touchedSpot, idx) {
-            final lineName = lines[idx].title;
+            final lineName = lines[touchedSpot.barIndex].title;
             final yValue = touchedSpot.y.asCompactDollars();
             return LineTooltipItem(
               '$lineName: $yValue',
-              TextStyle(color: lines[idx].color),
+              TextStyle(color: lines[touchedSpot.barIndex].color),
               // Chart library dictates that #tooltip_items == #touched_spots,
               //  so to show the date as a separate line, we append it to the
               //  last tooltip.
