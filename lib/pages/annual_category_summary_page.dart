663926cc29574dd2a049545e82d22fa389c4027c
Ethan Petuchowski <etahnp@gmail.com>
2022-11-05 20:37:26 -0500

    Partial feature: button that navigates to the new annual_category_summary_page.dart, which is currently empty.

---
+++ lib/pages/annual_category_summary_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AnnualCategorySummaryPage extends StatelessWidget {
+  const AnnualCategorySummaryPage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // TODO(feature): Table of annualized spending per category per year.
+      body: const Text('TODO(feature): Table'),
+    );
+  }
+}
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -5,6 +5,8 @@ import 'package:splurge/widgets/income_v_spending_line_chart.dart';
 import 'package:splurge/widgets/totals_card.dart';
 import 'package:splurge/widgets/transactions_review.dart';
 
+import 'annual_category_summary_page.dart';
+
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,7 +14,18 @@ class MainPage extends StatelessWidget {
       Expanded(
         flex: 3,
         child: Column(children: [
-          Row(children: [TotalsCard(), FilterCard()]),
+          Row(children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  _TableComparisonButton(),
+                  TotalsCard(),
+                ],
+              ),
+            ),
+            FilterCard(),
+          ]),
           Expanded(child: IncomeVsSpendingLineChart()),
         ]),
       ),
@@ -24,5 +37,22 @@ class MainPage extends StatelessWidget {
         ]),
       ),
     ]);
+  }
+}
+
+class _TableComparisonButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(18),
+      child: ElevatedButton(
+        onPressed: () => Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (ctx) => AnnualCategorySummaryPage(),
+          ),
+        ),
+        child: Text('Table: category comparison by year'),
+      ),
+    );
   }
 }
